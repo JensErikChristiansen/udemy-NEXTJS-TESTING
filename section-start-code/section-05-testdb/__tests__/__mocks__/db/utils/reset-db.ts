83a2c1259b93cb2135d120c43341f4e0e781d77d
Jens Erik Christiansen <undergroundviking@gmail.com>
2024-07-28 18:01:09 -0700

    section 5.44

---
+++ section-start-code/section-05-testdb/__tests__/__mocks__/db/utils/reset-db.ts
@@ -0,0 +1,26 @@
+import { fakeUserReservations } from './../../fakeData/userReservations';
+/* eslint-disable no-console */
+import { readFakeData } from '@/__tests__/__mocks__/fakeData/';
+import { filenames, writeJSONToFile } from '@/lib/db/db-utils';
+
+export async function resetDB() {
+  const isSafeToReset = process.env.NODE_ENV === 'test';
+
+  if (!isSafeToReset) {
+    console.log(
+      'WARNING: database reset unavailable outside test environment!',
+    );
+
+    return;
+  }
+
+  const { fakeShows, fakeBands, fakeUsers, fakeReservations } =
+    await readFakeData();
+
+  await Promise.all([
+    writeJSONToFile(filenames.bands, fakeBands),
+    writeJSONToFile(filenames.reservations, fakeReservations),
+    writeJSONToFile(filenames.shows, fakeShows),
+    writeJSONToFile(filenames.users, fakeUsers),
+  ]);
+}
