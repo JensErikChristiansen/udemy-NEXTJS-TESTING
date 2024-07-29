73b0313e0c66062de0804d642614a32b77dad37d
Jens Erik Christiansen <undergroundviking@gmail.com>
2024-07-29 16:07:39 -0700

    section 6.58

--- section-start-code/section-05-testdb/cypress/e2e/routes.cy.js
+++ section-start-code/section-05-testdb/cypress/e2e/routes.cy.js
@@ -3,3 +3,11 @@ it('displays correct heading when navigating to shows route', () => {
   cy.findByRole('button', { name: 'Shows' }).click();
   cy.findByRole('heading', { name: 'Upcoming Shows' }).should('exist');
 });
+
+it('displays correct heading when navigating to bands route', () => {
+  cy.visit('/');
+  cy.findByRole('button', { name: 'Bands' }).click();
+  cy.findByRole('heading', { name: 'Our Illustrious Performers' }).should(
+    'exist',
+  );
+});
